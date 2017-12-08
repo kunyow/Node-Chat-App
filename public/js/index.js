d0dc61fb16aa8aae5f1ee53d44ac0d446fa771c5
KunYou Wang <stam987654@gmail.com>
2017-12-08 13:14:23 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Brandon',
-    text: 'Yup, that works for me.'
-  });
 });
 
 socket.on('disconnect', function () {
